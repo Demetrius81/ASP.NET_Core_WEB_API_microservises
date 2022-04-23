78592c5a78a2034f49cb34271d1ac5645c3f7866
Dmitri <Shizimaniak@gmail.com>
2022-04-23 18:12:39 +0300

    Проект готов к сборке

--- Microservises/Educational_WEB_Service/Controllers/TempController.cs
+++ Microservises/Educational_WEB_Service/Controllers/TempController.cs
@@ -1,43 +1,142 @@
 ﻿using Lesson1.Model;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
 
 namespace Lesson1.Controllers
 {
+    /*
+    
+     V Написать свой контроллер и методы в нем, которые бы предоставляли следующую функциональность:
+     V Возможность сохранить температуру в указанное время
+     V Возможность отредактировать показатель температуры в указанное время
+     V Возможность удалить показатель температуры в указанный промежуток времени
+     V Возможность прочитать список показателей температуры за указанный промежуток времени
+
+     */
+
+    /// <summary>
+    /// Контроллер TempController
+    /// </summary>
     [Route("api/temperature")]
     [ApiController]
     public class TempController : ControllerBase
     {
+        /// <summary>
+        /// Поле для хранения пары дата - температура и доступа к ним
+        /// </summary>
         private readonly TempRepo _tempRepo;
 
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="tempRepo"></param>
         public TempController(TempRepo tempRepo)
         {
             _tempRepo = tempRepo;
         }
 
-        [HttpPost]
-        public IActionResult Create()
+        /// <summary>
+        /// Метод добавляет запись о температуре в определенное время
+        /// </summary>
+        /// <param name="dateTime">DateTime время</param>
+        /// <param name="temp">decimal температура</param>
+        /// <returns></returns>
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] DateTime dateTime, [FromQuery] decimal temp)
         {
+            _tempRepo.Add(dateTime, temp);
+
             return Ok();
         }
 
-        [HttpGet]
-        public IActionResult Read()
+        /// <summary>
+        /// Метод предоставляет запись о температуре по дате
+        /// </summary>
+        /// <param name="dateTime">DateTime дата</param>
+        /// <returns></returns>
+        [HttpGet("read")]
+        public IActionResult Read([FromQuery] DateTime dateTime)
         {
+            decimal temp = _tempRepo.Get(ref dateTime);
+
+            return Ok($"{dateTime}, {temp}");
+        }
+
+        /// <summary>
+        /// Метод предоставляет все записи о температуре
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("readall")]
+        public IActionResult ReadAll()
+        {
+            return Ok(_tempRepo.GetAll());
+        }
+
+        /// <summary>
+        /// Метод редактирует значение температуры по дате
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <param name="temp"></param>
+        /// <returns></returns>
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] DateTime dateTime, [FromQuery] decimal temp)
+        {
+            _tempRepo.Replace(dateTime, temp);
+
             return Ok();
         }
 
-        [HttpPut]
-        public IActionResult Update()
+        /// <summary>
+        /// Метод удаляет записm о температуре по указанному времени
+        /// </summary>
+        /// <param name="dateTime">DateTime дата и время</param>
+        /// <returns></returns>
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] DateTime dateTime)
         {
+            _ = _tempRepo.DateAndTemp.Remove(dateTime);
+
             return Ok();
         }
 
-        [HttpDelete]
-        public IActionResult Delete()
+        /// <summary>
+        /// Метод удаляет все записи о температуре и времени
+        /// </summary>
+        /// <returns></returns>
+        [HttpDelete("clear")]
+        public IActionResult Clear()
         {
+            _tempRepo.DateAndTemp.Clear();
+
             return Ok();
         }
 
+        /// <summary>
+        /// Метод удаляет показатель температуры в указанный промежуток времени
+        /// </summary>
+        /// <param name="dateTimeStart">DateTime начало интервала</param>
+        /// <param name="dateTimeEnd">DateTime конец интервала</param>
+        /// <returns></returns>
+        [HttpDelete("deletefortheinterval")]
+        public IActionResult DeleteForTheIntervale([FromQuery] DateTime dateTimeStart, [FromQuery] DateTime dateTimeEnd)
+        {
+            _tempRepo.RemoveInterval(dateTimeStart, dateTimeEnd);
+
+            return Ok();
+        }
+
+        /// <summary>
+        /// Метод выводит список показателей температуры за указанный промежуток времени
+        /// </summary>
+        /// <param name="dateTimeStart">DateTime начало интервала</param>
+        /// <param name="dateTimeEnd">DateTime конец интервала</param>
+        /// <returns></returns>
+        [HttpGet("readfortheinterval")]
+        public IActionResult ReadForTheIntervale([FromQuery] DateTime dateTimeStart, [FromQuery] DateTime dateTimeEnd)
+        {
+            return Ok($"{_tempRepo.ReadInterval(dateTimeStart, dateTimeEnd)}");
+        }
     }
 }
--- Microservises/Educational_WEB_Service/Model/TempRepo.cs
+++ Microservises/Educational_WEB_Service/Model/TempRepo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Lesson1.Model
 {
@@ -8,19 +9,22 @@ namespace Lesson1.Model
     /// </summary>
     public class TempRepo
     {
-        private Dictionary<DateTime, decimal> _DateAndTemp;
+        /// <summary>
+        /// Поле для хранения пары дата - температура
+        /// </summary>
+        private Dictionary<DateTime, decimal> _dateAndTemp;
 
         /// <summary>
         /// Свойство доступа к полю для хранения пары дата - температура
         /// </summary>
-        public Dictionary<DateTime, decimal> DateAndTemp { get => _DateAndTemp; set => _DateAndTemp = value; }
+        //public Dictionary<DateTime, decimal> DateAndTemp { get => _dateAndTemp; set => _dateAndTemp = value; }
 
         /// <summary>
         /// Конструктор класса
         /// </summary>
         public TempRepo()
         {
-            _DateAndTemp = new Dictionary<DateTime, decimal>();
+            _dateAndTemp = new Dictionary<DateTime, decimal>();
         }
 
         /// <summary>
@@ -30,7 +34,7 @@ namespace Lesson1.Model
         /// <param name="temp">decimal температура</param>
         public void Add(DateTime date, decimal temp)
         {
-            _DateAndTemp.Add(date, temp);
+            _dateAndTemp.Add(date, temp);
         }
 
         /// <summary>
@@ -40,9 +44,78 @@ namespace Lesson1.Model
         /// <returns>decimal температура</returns>
         public decimal Get(ref DateTime date)
         {
-            _ = _DateAndTemp.TryGetValue(date, out decimal temp);
-           
+            _ = _dateAndTemp.TryGetValue(date, out decimal temp);
+
             return temp;
         }
+
+        /// <summary>
+        /// Метод возвращает всю коллекцию значений дата - температура
+        /// </summary>
+        /// <returns>string вся коллекция значений дата - температура</returns>
+        public string GetAll()
+        {
+            string result = string.Empty;
+
+            foreach (DateTime key in _dateAndTemp.Keys)
+            {
+                result = result + $"{key}" + ", " + $"_DateAndTemp[key]" + "; ";
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Метод изменяет температуру по дате
+        /// </summary>
+        /// <param name="date">DateTime дата и время</param>
+        /// <param name="temp">decimal температура</param>
+        public void Replace(DateTime date, decimal temp)
+        {
+            if (_dateAndTemp.ContainsKey(date))
+            {
+                _dateAndTemp[date] = temp;
+            }
+        }
+
+        /// <summary>
+        /// Метод удаляет температуру в указанный интервал времени
+        /// </summary>
+        /// <param name="dateTimeStart">DateTime начало интервала</param>
+        /// <param name="dateTimeEnd">DateTime конец интервала</param>
+        public void RemoveInterval(DateTime dateTimeStart, DateTime dateTimeEnd)
+        {
+            foreach (DateTime key in GetInterval(dateTimeStart, dateTimeEnd))
+            {
+                _ = _dateAndTemp.Remove(key);
+            }
+        }
+
+        /// <summary>
+        /// Метод возвращает коллекцию значений дата - температура за указанный промежуток времени
+        /// </summary>
+        /// <param name="dateTimeStart">DateTime начало интервала</param>
+        /// <param name="dateTimeEnd">DateTime конец интервала</param>
+        /// <returns>string коллекция значений дата - температура</returns>
+        public string ReadInterval(DateTime dateTimeStart, DateTime dateTimeEnd)
+        {
+            string result = string.Empty;
+
+            foreach (DateTime key in GetInterval(dateTimeStart, dateTimeEnd))
+            {
+                result = result + $"{key}" + ", " + $"_DateAndTemp[key]" + "; ";
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Метод возвращает коллекцию ключей в интервале времени
+        /// </summary>
+        /// <param name="dateTimeStart">DateTime начало интервала</param>
+        /// <param name="dateTimeEnd">DateTime конец интервала</param>
+        /// <returns></returns>
+        private List<DateTime> GetInterval(DateTime dateTimeStart, DateTime dateTimeEnd) =>
+            _dateAndTemp.Keys.Where(key => key >= dateTimeStart && key <= dateTimeEnd).ToList();
     }
 }
