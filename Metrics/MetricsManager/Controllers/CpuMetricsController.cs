754069f15c665e342a41493a1431a002b2d3d7f1
Dmitri <Shizimaniak@gmail.com>
2022-04-26 11:06:50 +0300

    Создал проект, начал создавать контроллеры

---
+++ Metrics/MetricsManager/Controllers/CpuMetricsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace MetricsManager.Controllers
+{
+    /// <summary>
+    /// CPU контроллер
+    /// </summary>
+    [Route("api/cpu")]
+    [ApiController]
+    public class CpuMetricsController : ControllerBase
+    {
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent(
+            [FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+        [HttpGet("clister/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster(
+            [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+    }
+}
